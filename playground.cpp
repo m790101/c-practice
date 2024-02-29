8490f79e7a03fc2e730bb1366b03ce3aa638eb25
鄭皓文 HaoWen Cheng <haowen.cheng@tpisoftware.com>
2024-02-29 17:49:48 +0800

    update playground

--- playground.cpp
+++ playground.cpp
@@ -7,59 +7,90 @@
 #include <cstdlib>
 #include <random>
 #include <string>
+
+class Soldier
+{
+    virtual void attack(Soldier& enemy) = 0;
+};
+
 class Archer
 {
 protected:
-    int maxHealth;
+    int health;
     int range;
     int damage;
     std::string name = "archer";
     // int attackSpeed;
 public:
-    Archer(int maxHealth, int range, int damage)
+    Archer(int health, int range, int damage)
     {
-        this->maxHealth = maxHealth;
+        this->health = health;
         this->range = range;
         this->damage = damage;
     }
 
     void getDetails()
     {
-        std::cout << "maxHealth: " << maxHealth << std::endl;
+        std::cout << "health: " << health << std::endl;
         std::cout << "range: " << range << std::endl;
         std::cout << "damage: " << damage << std::endl;
     }
 
-    void attack()
+    int getHealth()
+    {
+        return health;
+    }
+
+    void attack(Archer &enemy)
     {
 
         std::random_device rd;  // a seed source for the random number engine
         std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
         std::uniform_int_distribution<> distrib(0, damage);
-      
-        std::cout << name <<" Attacking" << std::endl;
+        std::cout << name << " Attacking" << std::endl;
         int factor = distrib(gen);
         std::cout << "enemy got " << damage - factor << " damage" << std::endl;
+        enemy.health -= damage - factor;
     }
 };
 
-
-class ChineseArcher : public Archer{
-    public:
-
+class ChineseArcher : public Archer
+{
+public:
     // ChineseArcher():
-    ChineseArcher(int maxHealth, int range, int damage):Archer(maxHealth, range, damage){
+    ChineseArcher(int health, int range, int damage) : Archer(health, range, damage)
+    {
         name = "ChineseArcher"; // Setting name in the derived class
     }
 };
 
 int main()
 {
+    int rounds = 0;
     std::vector<int> foo = {1, 3};
     Archer archer1 = Archer(100, 10, 20);
-    archer1.attack();
-
     ChineseArcher archer2 = ChineseArcher(50, 8, 7);
-    archer2.attack();
+
+    while (archer2.getHealth() > 0)
+    {
+        rounds++;
+        archer1.attack(archer2);
+        if (archer2.getHealth() > 0)
+        {
+            std::cout << "---------enemy status:-----------" << std::endl;
+            std::cout << "remaining health: " <<archer2.getHealth() << std::endl;
+        } else {
+            std::cout << "enemy is dead" << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "total rounds:" << rounds << std::endl;
+    // archer1.attack(archer2);
+    // std::cout << "---------enemy status:-----------" << std::endl;
+    // //  std::cout << "--------------------" << std::endl;
+    // archer2.getDetails();
+    // std::cout << std::endl;
+    // archer2.attack();
     return 0;
 }
